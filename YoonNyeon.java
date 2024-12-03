8976b5687f6769a4a391a9f0e95273f543f174f5
Rabbokki <j55267035@gmail.com>
2024-12-03 11:49:42 +0900

    Add files via upload

---
+++ YoonNyeon.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class YoonNyeon {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//    int year = br.readline();
+
+}
